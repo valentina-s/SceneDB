38f6adff556c031b7a171fafefd81c95a60fb56b
Brandon Myers <bmyers1788@gmail.com>
2016-05-20 12:37:17 -0700

    Update start_postgres.sh

--- start_postgres.sh
+++ start_postgres.sh
@@ -1,2 +1,6 @@
+# get the postgres image
+docker pull postgres
+
+# start server
 PGPASSWORD=mysecretpassword
 docker run --name ashdb-postgres -e POSTGRES_PASSWORD=$PGPASSWORD -p 5432:5432 -d postgres
