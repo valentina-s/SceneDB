9843d76b61fd3ace486403d8d0d50511eed5f42d
valiseverywhere <val_is_everywhere@yahoo.com>
2016-09-09 19:05:44 -0700

    fixed docker

--- instance_setup.sh
+++ instance_setup.sh
@@ -19,7 +19,7 @@ pip install gcs-oauth2-boto-plugin
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
-sudo apt-get install docker-engine
+sudo apt-get install -y docker-engine
 
 # I might not need that
 # sudo vim /etc/apt/sources.list.d/docker.list
